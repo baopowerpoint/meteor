868fee4c6fa678e9d0775448686e2fbd69960d06
baopowerpoint <baopowerpoint@gmail.com>
2024-05-03 13:27:00 +0700

    change button

--- app/page.tsx
+++ app/page.tsx
@@ -25,33 +25,24 @@ export default function Home() {
           <div className="flex items-center gap-5 mt-5 justify-center ">
             <button
               onClick={handleIncrease}
-              className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 "
+              className="border border-slate-100 px-5 py-1 rounded-full "
             >
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ">
-                <p className="mx-5">Tăng</p>
-              </span>
+              <p className="mx-5">Tăng</p>
             </button>
             {meteorQuantity >= 200 && (
               <button
                 onClick={handleIncreaseByHundred}
-                className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 "
+                className="border border-slate-100 px-5 py-1 rounded-full "
               >
-                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ">
-                  <p className="mx-5">Tăng tiếp :)))</p>
-                </span>
+                <p className="mx-5">Tăng tiếp :)))</p>
               </button>
             )}
             <p>{meteorQuantity} ngôi sao</p>
             <button
               onClick={handleDecrease}
-              className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 "
+              className="border border-slate-100 px-5 py-1 rounded-full "
             >
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#fb7185_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ">
-                <p className="mx-5">Giảm</p>
-              </span>
+              <p className="mx-5">Giảm</p>
             </button>
           </div>
         </div>
